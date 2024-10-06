b9cff781809c078985e250a92b925f492ddef7ea
Shinkenuu <shinkenuu@gmail.com>
2024-10-06 18:04:17 +0100

    chore: removes unused vllm script

--- vllm.sh
+++
@@ -1,8 +0,0 @@
-docker run --runtime nvidia --gpus all \
-    -v ~/.cache/huggingface:/root/.cache/huggingface \
-    --env "HUGGING_FACE_HUB_TOKEN=<secret>" \
-    -p "8000:8000" \
-    --ipc=host \
-    vllm/vllm-openai:latest \
-    --model mistralai/Mistral-7B-v0.1
-    --build-arg torch_cuda_arch_list=""
