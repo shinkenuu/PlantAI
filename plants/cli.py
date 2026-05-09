81088d23f0b22c55169135d4f0b2f906ed3e47de
Shinkenuu <shinkenuu@gmail.com>
2026-05-09 17:13:31 +0100

    fix and cleanup snapshotting plants sensors readings

--- plants/cli.py
+++ plants/cli.py
@@ -5,23 +5,17 @@ from pathlib import Path
 from typing import Annotated
 
 import typer
-from rich.console import Console
-from rich.logging import RichHandler
-from rich.progress import Progress, SpinnerColumn, TextColumn
 
 from plants.repositories import get_plant_repository
 from plants.schemas import Plant
 
-# Setup rich console
-console = Console()
 logging.basicConfig(
     level="INFO",
     format="%(message)s",
     datefmt="[%X]",
-    handlers=[RichHandler(rich_tracebacks=True, console=console)],
 )
 
-app = typer.Typer(rich_markup_mode="rich")
+app = typer.Typer()
 
 
 def _append_to_sensor_reading_log(plants: list[Plant], sensor_reading_path: Path):
@@ -45,23 +39,18 @@ def _append_to_sensor_reading_log(plants: list[Plant], sensor_reading_path: Path
 @app.command()
 def read_plants_sensors(
     backend: Annotated[str, typer.Option(help="Repository backend")] = "arduino",
-    pins_path: Annotated[str, typer.Option(help="Arduino pinout")] = "plants/pins.json",
+    pins_path: Annotated[Path, typer.Option(help="Arduino pinout")] = Path(
+        "plants/pins.json"
+    ),
     log_path: Annotated[Path, typer.Option(help="Log path")] = Path("sensors.jsonl"),
 ):
-    with Progress(
-        SpinnerColumn(),
-        TextColumn("[progress.description]{task.description}"),
-        transient=True,
-    ) as progress:
-        progress.add_task(description=f"Reading plants from {backend}...", total=None)
+    plant_repository = get_plant_repository(backend=backend, pins_path=pins_path)
+    plants = plant_repository.list_plants()
 
-        plant_repository = get_plant_repository(backend=backend, pins_path=pins_path)
-        plants = plant_repository.list_plants()
+    if log_path:
+        _append_to_sensor_reading_log(plants, log_path)
 
-        if log_path:
-            _append_to_sensor_reading_log(plants, log_path)
-
-        return plants
+    return plants
 
 
 if __name__ == "__main__":
--- scripts/snapshot_plant_sensor_readings.sh
+++ scripts/snapshot_plant_sensor_readings.sh
@@ -1,5 +1,1 @@
-WORKDIR="/home/shinkenuu/Projects/PlantAI/"
-SENSOR_READING_FILE_PATH="sensor_readings.jsonl"
-
-cd $WORKDIR
 uv run plants/cli.py --pins-path="plants/pins.json"
